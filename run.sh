1b36fa904fc1ab3944cb8fe3756fda03ca54427d
Chris Proto <cproto@etsy.com>
2024-05-24 15:51:15 -0400

    retry

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ main() {
     vhost=$1
     host=$2
 
-    GOBUSTER="./gobuster dir --threads 5 --timeout 15s --status-codes-blacklist '404,400,403' -H 'Host: $vhost' -u http://$host"
+    GOBUSTER="./gobuster dir --retry --threads 5 --timeout 15s --status-codes-blacklist '404,400,403' -H 'Host: $vhost' -u http://$host"
 
     bash -c "$GOBUSTER -w ./wordlists/LFI-Jhaddix.txt -o reports/lfi-fuzzing.txt"
     bash -c "$GOBUSTER -w ./wordlists/LFI-gracefulsecurity-linux.txt -o reports/lfi-linux.txt"
